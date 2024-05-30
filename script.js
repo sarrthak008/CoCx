521ef77e2cc9fc832d12fddc6724c72e710aac8d
sarrthak008 <sarthaknavale08@gmail.com>
2024-05-30 12:12:36 +0530

    add animations on boxes of companys

--- script.js
+++ script.js
@@ -32,3 +32,28 @@ TimeLine.from('.moving-txt2 h1',{
 
     }
  },"same")
+
+
+let TimeLine2 = gsap.timeline()
+
+TimeLine2.from("#left span",{
+    transform:`translateY(0%)`,
+    duration:15,
+    repeat:-1,
+    ease:"none",
+ },"same1")
+
+
+
+
+ 
+TimeLine2.to("#right span",{
+    transform:`translateY(0%)`,
+    duration:15,
+    repeat:-1,
+    ease:"none",
+ },"same1")
+
+
+ 
+ 
